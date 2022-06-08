cb1e17ec84dbe65a0ea3d5a59de485aa07a2c4de
Miguel-202 <62391423+Miguel-202@users.noreply.github.com>
2022-06-08 18:38:46 -0400

    Button Testnig
    Playing arround with buttons to display on the calculator

--- Main.cpp
+++ Main.cpp
@@ -17,61 +17,61 @@ Main::Main() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(600,100), wxSize
 	operations = new wxTextCtrl(this, 10040, "", wxPoint(currentX, currentY), wxSize(xSize * 4, ySize ), wxTE_READONLY); currentY += ySize ;
 	//wxListBox* result = new wxListBox(this, 10041, wxPoint(currentX, currentY), wxSize(xSize * 4, ySize / 2)); currentY += ySize / 2;
 
-	power = new wxButton(this, 10020, "ON / OFF", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	power = new wxButton(this, 20, "ON / OFF", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	power->SetBackgroundColour(*wxRED);
-	mod = new wxButton(this, 10037, "MOD", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	mod = new wxButton(this, 37, "MOD", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	mod->SetBackgroundColour(*wxLIGHT_GREY);
-	negative = new wxButton(this, 10018, "NEGATIVE", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	negative = new wxButton(this, 18, "NEGATIVE", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	negative->SetBackgroundColour(*wxLIGHT_GREY);
-	clear = new wxButton(this, 10017, "CLEAR", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
+	clear = new wxButton(this, 17, "CLEAR", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
 	clear->SetBackgroundColour(*wxRED);
 	currentY += ySize;
 
-	seven = new wxButton(this, 10055, "7", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	seven = new wxButton(this, 55, "7", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//seven->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	eigth = new wxButton(this, 10056, "8", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	eigth = new wxButton(this, 56, "8", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//eigth->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	nine = new wxButton(this, 10057, "9", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	nine = new wxButton(this, 57, "9", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//nine->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	divide = new wxButton(this, 10047, "/", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
+	divide = new wxButton(this, 47, "/", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
 	divide->SetBackgroundColour(*wxLIGHT_GREY);
 	currentY += ySize;
 
-	four = new wxButton(this, 10052, "4", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	four = new wxButton(this, 52, "4", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//four->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	five = new wxButton(this, 10053, "5", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	five = new wxButton(this, 53, "5", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//five->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	six = new wxButton(this, 10054, "6", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	six = new wxButton(this, 54, "6", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//six->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	multiply = new wxButton(this, 10042, "*", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
+	multiply = new wxButton(this, 42, "*", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
 	multiply->SetBackgroundColour(*wxLIGHT_GREY);
 	currentY += ySize;
 
-	one = new wxButton(this, 10049, "1", wxPoint(currentX,currentY), wxSize(xSize,ySize)); currentX += xSize;
+	one = new wxButton(this, 49, "1", wxPoint(currentX,currentY), wxSize(xSize,ySize)); currentX += xSize;
 	//one->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	two = new wxButton(this, 10050, "2", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	two = new wxButton(this, 50, "2", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//two->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	three = new wxButton(this, 10051, "3", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	three = new wxButton(this, 51, "3", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//three->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	minus = new wxButton(this, 10045, "-", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
+	minus = new wxButton(this, 45, "-", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
 	minus->SetBackgroundColour(*wxLIGHT_GREY);
 	currentY += ySize;
 
-	zero = new wxButton(this, 10048, "0", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	zero = new wxButton(this, 48, "0", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//zero->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	dot = new wxButton(this, 10046, ".", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	dot = new wxButton(this, 46, ".", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	//dot->Bind(wxEVT_COMMAND_BUTTON_CLICKED, &Main::OnButtonNumClicked, this);
-	equals = new wxButton(this, 10061, "=", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	equals = new wxButton(this, 61, "=", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	equals->SetBackgroundColour(*wxRED);
-	plus = new wxButton(this, 10043, "+", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
+	plus = new wxButton(this, 43, "+", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX = 0;
 	plus->SetBackgroundColour(*wxLIGHT_GREY);
 	currentY += ySize;
 
-	decimal = new wxButton(this, 10030, "DECIMAL", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	decimal = new wxButton(this, 30, "DECIMAL", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	decimal->SetBackgroundColour(*wxLIGHT_GREY);
-	binary = new wxButton(this, 10031, "BIN", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	binary = new wxButton(this, 31, "BIN", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	binary->SetBackgroundColour(*wxLIGHT_GREY);
-	hex = new wxButton(this, 10032, "HEX", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
+	hex = new wxButton(this, 32, "HEX", wxPoint(currentX, currentY), wxSize(xSize, ySize)); currentX += xSize;
 	hex->SetBackgroundColour(*wxLIGHT_GREY);
 }
 
